b9277c0ffdc11f81f9a149cfbf52d00510c00d87
Michael Fewkes <mdfewkes@gmail.com>
2021-02-10 17:12:12 -0700

    Player faces the direction they move

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
-export var ACCELERATION = 1500
+export var ACCELERATION = 1000
 export var MAX_SPEED = 500
 export var ROLL_SPEED = 120
-export var FRICTION = 1000
+export var FRICTION = 1500
 
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _process(delta):
 	input_vector = Vector2.ZERO
@@ -16,6 +20,7 @@ func _process(delta):
 	
 	if !input_vector.is_equal_approx(Vector2.ZERO):
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		animationTree.set("parameters/Idle/blend_position", input_vector)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
