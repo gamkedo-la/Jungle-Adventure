a0e62df38d02e40f45d314172d4ba60d16e01f9d
Michael Fewkes <mdfewkes@gmail.com>
2021-02-10 17:52:25 -0700

    Player strike box

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
+enum {
+	MOVE,
+	STRIKE
+}
+
 export var ACCELERATION = 1000
 export var MAX_SPEED = 500
 export var ROLL_SPEED = 120
 export var FRICTION = 1500
 
+var state = MOVE
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
 
@@ -13,6 +19,13 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _process(delta):
+	match state:
+		MOVE:
+			state_move(delta)
+		STRIKE:
+			state_strike(delta)
+
+func state_move(delta):
 	input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -21,8 +34,32 @@ func _process(delta):
 	if !input_vector.is_equal_approx(Vector2.ZERO):
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Strike/blend_position", input_vector)
+		animationState.travel("Idle")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
+	if velocity.is_equal_approx(Vector2.ZERO):	
+		animationState.travel("Idle")
+	if Input.is_action_just_pressed("action"):
+		state = STRIKE
+	
 	velocity = move_and_slide(velocity)
+	
+	
+func state_strike(delta):
+	animationState.travel("Strike")
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = move_and_slide(velocity)
+	
+	
+
+func set_state_to_move():
+	state = MOVE
+	
+	
+	
+	
+	
+	
 	
