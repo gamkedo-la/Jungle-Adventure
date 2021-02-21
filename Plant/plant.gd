3a3b5d8427e92622fb4c940143652eb0a38ed0fe
Michael Fewkes <mdfewkes@gmail.com>
2021-02-21 15:52:59 -0700

    Better bush and tree growth

--- Plant/plant.gd
+++ Plant/plant.gd
@@ -6,12 +6,18 @@ var hp = maxHP
 
 onready var collisionShape = $CollisionShape2D
 onready var sprite = $Sprite
+onready var animationPlayer = $AnimationPlayer
 
 func _ready():
 	hp = maxHP
+	
+func add_hp(to_add: int):
+	hp = clamp(to_add + hp, 0, maxHP)
 
 func _on_Area2D_area_entered(_area):
+	if hp <= 0:
+		return
 	hp -= 1
 	if hp <= 0:
-		sprite.frame = 1
-		collisionShape.set_deferred("disabled", true)
+		animationPlayer.stop()
+		animationPlayer.play("Grow")
