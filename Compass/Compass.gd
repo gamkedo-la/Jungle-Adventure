89d86fc1c8924c8ff0263f87055c16666477c393
MrPhil <mrphil@mrphilgames.com>
2021-03-08 09:41:35 -0600

    Added skeleton for Compass

---
+++ Compass/Compass.gd
@@ -0,0 +1,15 @@
+extends CenterContainer
+
+public AnimatedSprite Pointer; // The part that points "north"
+public var North; // The Gameobject? the compass points too
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	// Do some math to recalculate the Pointer orientation
+	pass
