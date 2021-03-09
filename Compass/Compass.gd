9c0b3f8224764e805e9a5e1b2413c07b88e91fa8
MrPhil <mrphil@mrphilgames.com>
2021-03-09 10:03:25 -0600

    Compass: Added method to set "north"

--- Compass/Compass.gd
+++ Compass/Compass.gd
@@ -1,8 +1,10 @@
 extends CenterContainer
 
-public AnimatedSprite Pointer; // The part that points "north"
-public var North; // The Gameobject? the compass points too
-
+# export(type, min, max, step) https://docs.godotengine.org/en/stable/getting_started/scripting/gdscript/gdscript_exports.html
+# These are complaining about not being a resource type ??
+#export(AnimatedSprite) var Pointer;  # The part that points "north"
+#export(Position2D) var NorthPosition = ; # The compass points at this position aka North
+var NorthPosition:Position2D; # The compass points at this position aka North
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +13,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	// Do some math to recalculate the Pointer orientation
+	# Do some math to recalculate the Pointer orientation
 	pass
+	
+# Not sure what I've done wrong declaring these param
+#func SetNorthPosition(Position2D northPosition):
+func SetNorthPosition(northPosition):
+	NorthPosition = northPosition
