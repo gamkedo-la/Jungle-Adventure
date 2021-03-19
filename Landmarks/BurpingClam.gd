e46278901d26e658eb3b2aadb2a40f13ba5eb377
Oli Brock <kbshields@gmail.com>
2021-03-18 22:05:33 -0400

    Redrew Burping Clam Landmark and animated new sprites.  Rocks and plants to come later.

--- Landmarks/BurpingClam.gd
+++ Landmarks/BurpingClam.gd
@@ -3,6 +3,6 @@ extends Node2D
 onready var animationPlayer = $AnimationPlayer
 onready var bubbles =  $ClamBubbles
 func _ready():
-	animationPlayer.play("Open and Close")
+	animationPlayer.play("onc")
 	
 
