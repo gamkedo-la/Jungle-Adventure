edcf50e065bb5859afc1920579c874e93521192d
Oli Brock <kbshields@gmail.com>
2021-04-03 12:25:26 -0400

    fix (bush/tree): unified Hit animation naming, fixed add_hp issue that let immovable bushes/trees grow,

--- Plant/plant.gd
+++ Plant/plant.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 export (AudioStreamRandomPitch) var sfxChop
 export (AudioStreamRandomPitch) var sfxHit
 
+var maxHP = 1
 var hp
 var hitCount
 
@@ -14,9 +15,14 @@ onready var audioStreamPlayer = $AudioStreamPlayer2D
 func _ready():
 	hitCount = 0
 	if "Tree" in name:
+		maxHP = 4
 		hp = 4
 	else:
+		maxHP = 1
 		hp= 1
+		
+func add_hp(to_add: int):
+	hp = clamp(to_add + hp, 0, maxHP)
 	
 func _on_Area2D_area_entered(_area):
 	if hp <= 0:
@@ -25,16 +31,17 @@ func _on_Area2D_area_entered(_area):
 	hitCount += 1
 	#Var printing for testing purposes
 	#print("Hit number: " + str(hitCount))
-	#print(name + "current HP: " + str(hp))
+	#print(name + "current HP: " + str(hp))	
 	animationPlayer.play("Hit " + str(hitCount))
 	animationPlayer.queue("Idle Lv"+str(hp))
 	if hp <= 0:
+		hitCount = 0
 		#Depending on plant type, we will use a cut animation or another
 		if "Bush" in name:
 			animationPlayer.play("Grow")
 			#If we wanted to have the bush hit animation - removed as it mismatches sound.
-			#animationPlayer.play("Hit Lv1")
-			#animationPlayer.queue("Grow")
+			animationPlayer.play("Hit 1")
+			animationPlayer.queue("Grow")
 			audioStreamPlayer.stream = sfxChop
 		else:
 			animationPlayer.play("Hit 4")
