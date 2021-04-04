f119b12c4368cb828b55fe408dbf335410a2300a
MrPhil <mrphil@mrphilgames.com>
2021-04-04 18:57:29 -0500

    Compass Juice is more jittery now.

--- Compass/Compass.gd
+++ Compass/Compass.gd
@@ -5,16 +5,18 @@ extends Container
 # They will also be available for editing in the property editor.
 # https://docs.godotengine.org/en/stable/getting_started/scripting/gdscript/gdscript_exports.html
 export(Vector2) var NorthPosition = Vector2.ZERO  # The compass points at this position aka North
-export(float) var wiggleRange := 0.05
-
-onready var rng := RandomNumberGenerator.new()
+export(float) var WiggleRange := 0.05
+export(float) var WiggleDelaySecondsRange := 0.5
 
 var PlayerPosition = Vector2.ZERO
+var Wiggle = 0;
 
 # onready var my_label = get_node("MyLabel") 
 # A scene's subnodes can't be accessed until _ready() is entered. The onready keyword, defers variable initialization until _ready() is called.
 # https://docs.godotengine.org/en/stable/getting_started/scripting/gdscript/gdscript_basics.html#onready-keyword
 onready var Needle := get_node("Needle")
+onready var WiggleTimer := $WiggleTimer
+onready var rng := RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,10 +27,12 @@ func _process(_delta):
 	PlayerPosition = Global.player_position
 	
 	# Randomize a tiny bit so the needle has some juice
-	var wiggle = rng.randf_range(-wiggleRange, wiggleRange)
+	if(WiggleTimer.is_stopped()) :
+		Wiggle = rng.randf_range(-WiggleRange, WiggleRange)
+		WiggleTimer.start(rng.randf_range(0, WiggleDelaySecondsRange))
 	
 	# Do some math to recalculate the Pointer orientation
-	Needle.rotation = Global.player_position.angle_to_point(NorthPosition) - 1.570796 + wiggle
+	Needle.rotation = Global.player_position.angle_to_point(NorthPosition) - 1.570796 + Wiggle
 	
 # Not sure what I've done wrong declaring these param
 #func SetNorthPosition(Position2D northPosition):
