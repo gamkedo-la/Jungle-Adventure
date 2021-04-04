12f40639699e45e051ae7eea3951ba57524a1627
MrPhil <mrphil@mrphilgames.com>
2021-04-03 20:33:30 -0500

    Compass: Add some random wiggle motion to the needle.

--- Compass/Compass.gd
+++ Compass/Compass.gd
@@ -5,6 +5,10 @@ extends Container
 # They will also be available for editing in the property editor.
 # https://docs.godotengine.org/en/stable/getting_started/scripting/gdscript/gdscript_exports.html
 export(Vector2) var NorthPosition = Vector2.ZERO  # The compass points at this position aka North
+export(float) var wiggleRange := 0.05
+
+onready var rng := RandomNumberGenerator.new()
+
 var PlayerPosition = Vector2.ZERO
 
 # onready var my_label = get_node("MyLabel") 
@@ -14,14 +18,17 @@ onready var Needle := get_node("Needle")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rng.randomize()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	PlayerPosition = Global.player_position
-		
+	
+	# Randomize a tiny bit so the needle has some juice
+	var wiggle = rng.randf_range(-wiggleRange, wiggleRange)
+	
 	# Do some math to recalculate the Pointer orientation
-	Needle.rotation = Global.player_position.angle_to_point(NorthPosition) - 1.570796
+	Needle.rotation = Global.player_position.angle_to_point(NorthPosition) - 1.570796 + wiggle
 	
 # Not sure what I've done wrong declaring these param
 #func SetNorthPosition(Position2D northPosition):
